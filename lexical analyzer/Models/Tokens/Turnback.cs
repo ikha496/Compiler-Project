111f29e9a61d69b26fcfd777967cdc94d8256668
Shreen Mohamed <shereenmohamed5555@gmail.com>
2022-05-19 21:26:09 +0200

    classes_for_second_Group_of_tokens

---
+++ lexical analyzer/Models/Tokens/Turnback.cs
@@ -0,0 +1,31 @@
+﻿namespace lexical_analyzer.Models.Tokens
+{
+    public class Turnback
+    {
+        public static string Tokenname = "Turnback";
+
+        private List<char> characters = new List<char> { 'T','u','r','n','b','a','c','k' };
+        public bool isValid(string keyword, int stateNumber)
+        {
+            if (keyword.Length != characters.Count)
+            {
+                CompilerManager.instance.setState(stateNumber);
+                return false;
+            }
+            for (int i = 0; i < keyword.Length; i++)
+            {
+                if (keyword[i] == characters[i])
+                {
+                    CompilerManager.instance.state++;
+                }
+                else
+                {
+                    CompilerManager.instance.setState(stateNumber);
+                    return false;
+                }
+            }
+            CompilerManager.instance.validToken(Tokenname, keyword);
+            return true;
+        }
+    }
+}
