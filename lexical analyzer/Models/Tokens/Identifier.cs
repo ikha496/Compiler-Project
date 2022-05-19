26b90efe3b3ce370d8677c6fa8a45c72bfba3888
Shreen Mohamed <shereenmohamed5555@gmail.com>
2022-05-19 22:00:09 +0200

    Identifier_class

---
+++ lexical analyzer/Models/Tokens/Identifier.cs
@@ -0,0 +1,23 @@
+﻿namespace lexical_analyzer.Models.Tokens
+{
+    public class Identifier
+    {
+        public string Tokenname = "Identifier";
+        private List<char> digits = new List<char> { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+        private List<char> spicailChracter = new List<char> { '#', '$', '%', ';', '@', '^', '&', '*', '(', ')', '+', '*', '/', '>', '<', '=', '-', '{', '}', '[', ']' };
+
+        public bool isValid(string keyword, int stateNumber)
+        {
+            if (digits.Contains(keyword[0]) || spicailChracter.Contains(keyword[0]))
+            {
+                CompilerManager.instance.setState(stateNumber);
+                return false;
+            }
+            else
+            {
+                CompilerManager.instance.validToken(Tokenname, keyword);
+                return true;
+            }
+        }
+    }
+}
