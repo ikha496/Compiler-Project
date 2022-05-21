9013a91f5983f138db44ecde50b8c84769253ab1
Osamaali2001 <oa238388@gmail.com>
2022-05-21 11:09:41 +0200

    .

--- lexical analyzer/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace lexical_analyzer.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
